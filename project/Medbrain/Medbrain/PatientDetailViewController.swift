bc9182f95546f581e0b77b1f8fdbd5af54ebf535
Simon Anreiter <simon.anreiter@i-mobility.at>
2016-08-16 23:57:45 +0200

    Added PatientDetailViewController

---
+++ project/Medbrain/Medbrain/PatientDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PatientDetailViewController.swift
+//  Medbrain
+//
+//  Created by Simon Anreiter on 16/08/16.
+//  Copyright © 2016 Simon Anreiter. All rights reserved.
+//
+
+import UIKit
+
+class PatientDetailViewController: UIViewController {
+    
+    
+    
+    
+}
