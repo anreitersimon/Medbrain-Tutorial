1926f71ffc83cb2a7b79f2d7eb81158443f30080
Simon Anreiter <simon.anreiter@i-mobility.at>
2016-08-16 20:37:00 +0200

    Added `PatientMedicationsViewController`

---
+++ project/Medbrain/Medbrain/PatientMedicationsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PatientMedicationsViewController.swift
+//  Medbrain
+//
+//  Created by Simon Anreiter on 16/08/16.
+//  Copyright © 2016 Simon Anreiter. All rights reserved.
+//
+
+import UIKit
+
+class PatientMedicationsViewController: UITableViewController {
+
+}
