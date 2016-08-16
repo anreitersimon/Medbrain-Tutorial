950fac5d908b95fc90624c69d3786e669c8b05b2
Simon Anreiter <simon.anreiter@i-mobility.at>
2016-08-16 20:24:01 +0200

    Implemented `PatientSignInViewController` functionality

--- project/Medbrain/Medbrain/PatientSignInViewController.swift
+++ project/Medbrain/Medbrain/PatientSignInViewController.swift
@@ -17,8 +17,57 @@ class PatientSignInViewController: UIViewController {
     @IBOutlet var submitButton: UIButton!
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
 
+    ///Will be called after the Sign-In successfully finished
+    ///Note: user cannot leave screen without successfully signing in
+    var completionHandler: SignInCompletionHandler?
+
+    ///generate `LogInCredentials` from the information entered
+    ///- returns: nil if the entered credentials are incomplete or invalid
+    func generateCredentials() -> LogInCredentials? {
+        //when sign-in is implemented properly this has to be adjusted
+
+        //Always return the default credentials for the test-user
+        return LogInCredentials.defaultCredentials
+    }
+
+    func showError(message message: String) {
+        let alertController = UIAlertController(title: message, message: nil, preferredStyle: .Alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+
+        presentViewController(alertController, animated: true, completion: nil)
+    }
 
     @IBAction func submitButtonPressed(sender: AnyObject?) {
-        //will be implemented later
+        //verify that the entered information is valid
+        guard let credentials = generateCredentials() else {
+            showError(message: "Entered Information is incomplete or invalid")
+            return
+        }
+
+        //disable textFields so user cannot edit information while it is being submitted
+        emailTextfield.userInteractionEnabled = false
+        passwordTextfield.userInteractionEnabled = false
+        submitButton.enabled = false
+        activityIndicator.startAnimating()
+
+        //attempt sign-in
+        SessionManager.shared.logIn(credentials) { (result) in
+
+            //reenable textfields
+            self.emailTextfield.userInteractionEnabled = true
+            self.passwordTextfield.userInteractionEnabled = true
+            self.submitButton.enabled = true
+            self.activityIndicator.stopAnimating()
+
+            //check result
+            switch result {
+            case .Success(let patient):
+                //Successful: call completionHandler
+                self.completionHandler?(patient: patient)
+            case .Error(_):
+                //Failed: Display Error Message
+                self.showError(message: "Something went wrong")
+            }
+        }
     }
 }
